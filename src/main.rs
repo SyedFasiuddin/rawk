e99aeaf55dcb55f0049ab95e8a0db8962823a2a3
Syed Fasiuddin <66054777+SyedFasiuddin@users.noreply.github.com>
2023-05-22 14:38:09 +0530

    list all tokens, skeleton of lexer

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,78 @@ struct Args {
     file: Vec<String>
 }
 
+enum Token {
+    // Keywords
+    If,
+    Else,
+    While,
+    For,
+    Function,
+    In,
+    Do,
+    Break,
+    Continue,
+    Print,
+    Printf,
+    Return,
+    Next,
+    Nextfile,
+    Delete,
+    Exit,
+
+    // Braces
+    RightParen,
+    LeftParen,
+    RightCurly,
+    LeftCurly,
+    RightSquare,
+
+    // Math symbols,
+    Plus,
+    Minus,
+    Multiply,
+    Divide,
+    Modulus,
+    Pow,
+    DoublePlus,
+    DoubleMinus,
+    PlusEqual,
+    MinusEqual,
+    TimesEqual,
+    DivEqual,
+    ModEqual,
+    PowEqual,
+
+    // Logical symbols,
+    Not,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+    Equal,
+    NotEqual,
+
+    // ?:
+}
+
+struct Lexer {
+    str: String,
+    idx: usize,
+}
+
+impl Lexer {
+    fn next(&mut self) -> Token {
+        Token::Return
+    }
+
+    fn peek(&self) -> Token {
+        Token::Return
+    }
+
+    fn skip_whitespcae(&mut self) {
+    }
+}
+
 fn main() {
     let _args = Args::parse();
 }
