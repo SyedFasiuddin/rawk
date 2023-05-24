def30729e75247bdfb2cc1f5068db18325288b68
Syed Fasiuddin <66054777+SyedFasiuddin@users.noreply.github.com>
2023-05-24 20:48:57 +0530

    add more parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code, unused_variables)]
+
 use clap::Parser;
 
 #[derive(Parser, Debug)]
@@ -36,6 +38,9 @@ enum Token {
     LeftSquare,
     RightSquare,
 
+    LeftAngle,
+    RightAngle,
+
     // Math symbols,
     Plus,
     Minus,
@@ -67,6 +72,9 @@ enum Token {
     End,
     String(String),
     RegExp(String),
+    Identifier(String),
+    Comma,
+    SemiColon,
     Other(String),
 }
 
@@ -87,28 +95,133 @@ impl Lexer {
         let mut word = String::new();
 
         while self.idx < self.chars.len() {
+            let c = self.chars[self.idx];
             if self.is_whitespace() {
                 self.skip_whitespace();
                 break;
-            } else if self.chars[self.idx] == '#' {
+            } else if c == '#' {
                 self.skip_comment_line();
-            } else if self.chars[self.idx] == '"' {
+            } else if c == '"' {
                 self.idx += 1; // skip first '"'
                 let str = self.handle_string();
                 self.skip_whitespace();
                 return Some(Token::String(str));
-            } else if self.chars[self.idx] == '/' {
+            } else if c == '/' {
                 self.idx += 1; // skip first '/'
                 let re = self.handle_regex();
                 self.skip_whitespace();
                 return Some(Token::RegExp(re));
+            } else if c == '+' {
+                self.idx += 1;
+                let c_next = self.chars[self.idx];
+                if c_next == '+' {
+                    self.idx += 1;
+                    return Some(Token::DoublePlus);
+                } else if c_next == '=' {
+                    self.idx += 1;
+                    return Some(Token::PlusEqual);
+                } else {
+                    return Some(Token::Plus);
+                }
+            } else if c == '-' {
+                self.idx += 1;
+                let c_next = self.chars[self.idx];
+                if c_next == '-' {
+                    self.idx += 1;
+                    return Some(Token::DoubleMinus);
+                } else if c_next == '=' {
+                    self.idx += 1;
+                    return Some(Token::MinusEqual);
+                } else {
+                    return Some(Token::Minus);
+                }
+            } else if c == '*' {
+                self.idx += 1;
+                let c_next = self.chars[self.idx];
+                if c_next == '=' {
+                    self.idx += 1;
+                    return Some(Token::TimesEqual);
+                } else {
+                    return Some(Token::Multiply);
+                }
+            } else if c == '/' {
+                self.idx += 1;
+                let c_next = self.chars[self.idx];
+                if c_next == '=' {
+                    self.idx += 1;
+                    return Some(Token::DivEqual);
+                } else {
+                    return Some(Token::Divide);
+                }
+            } else if c == '%' {
+                self.idx += 1;
+                let c_next = self.chars[self.idx];
+                if c_next == '=' {
+                    self.idx += 1;
+                    return Some(Token::ModEqual);
+                } else {
+                    return Some(Token::Modulus);
+                }
+            } else if c == '^' {
+                self.idx += 1;
+                let c_next = self.chars[self.idx];
+                if c_next == '=' {
+                    self.idx += 1;
+                    return Some(Token::PowEqual);
+                } else {
+                    return Some(Token::Pow);
+                }
+            } else if c == ',' {
+                if word.is_empty() {
+                    self.idx += 1;
+                    return Some(Token::Comma);
+                } else {
+                    break;
+                }
+            } else if c == ';' {
+                self.idx += 1;
+                return Some(Token::SemiColon);
+            } else if c == '(' {
+                self.idx += 1;
+                return Some(Token::LeftParen);
+            } else if c == ')' {
+                self.idx += 1;
+                return Some(Token::RightParen);
+            } else if c == '[' {
+                if word.is_empty() {
+                    self.idx += 1;
+                    return Some(Token::LeftSquare);
+                } else {
+                    word.push(c);
+                    self.idx += 1;
+                }
+            } else if c == ']' {
+                if word.is_empty() {
+                    self.idx += 1;
+                    return Some(Token::RightSquare);
+                } else {
+                    word.push(c);
+                    self.idx += 1;
+                }
+            } else if c == '{' {
+                self.idx += 1;
+                return Some(Token::LeftCurly);
+            } else if c == '}' {
+                self.idx += 1;
+                return Some(Token::RightCurly);
+            } else if c == '<' {
+                self.idx += 1;
+                return Some(Token::LeftAngle);
+            } else if c == '>' {
+                self.idx += 1;
+                return Some(Token::RightAngle);
             } else {
-                word.push(self.chars[self.idx]);
+                word.push(c);
                 self.idx += 1;
             }
         }
 
-        if self.idx > self.chars.len() {
+        if self.idx >= self.chars.len() {
             return None;
         }
 
@@ -131,29 +244,8 @@ impl Lexer {
             "delete" => Some(Token::Delete),
             "exit" => Some(Token::Exit),
 
-            "(" => Some(Token::LeftParen),
-            ")" => Some(Token::RightParen),
-
             "{" => Some(Token::LeftCurly),
             "}" => Some(Token::RightCurly),
-
-            "[" => Some(Token::LeftSquare),
-            "]" => Some(Token::RightSquare),
-
-            "+" => Some(Token::Plus),
-            "-" => Some(Token::Minus),
-            "*" => Some(Token::Multiply),
-            "/" => Some(Token::Divide),
-            "%" => Some(Token::Modulus),
-            "^" => Some(Token::Pow),
-            "++" => Some(Token::DoublePlus),
-            "--" => Some(Token::DoubleMinus),
-            "+=" => Some(Token::PlusEqual),
-            "-=" => Some(Token::MinusEqual),
-            "*=" => Some(Token::TimesEqual),
-            "/=" => Some(Token::DivEqual),
-            "%=" => Some(Token::ModEqual),
-            "^=" => Some(Token::PowEqual),
 
             "!" => Some(Token::Not),
             ">" => Some(Token::Greater),
@@ -166,8 +258,8 @@ impl Lexer {
             "BEGIN" => Some(Token::Begin),
             "END" => Some(Token::End),
 
-            // word => Some(Token::Other(word.to_string())),
-            _ => None,
+            word if word == "" => self.next(),
+            word => Some(Token::Identifier(word.to_string())),
         }
     }
 
@@ -249,6 +341,29 @@ BEGIN { print "Analysis of \"li\"" }
 /li/  { ++n }
 END   { print "\"li\" appears in", n, "records." }
         "#;
+
+    let awk = r##"
+BEGIN {
+  print "/* DO NOT EDIT THIS FILE - it is machine generated */";
+  print "#ifndef TL_CONSTANTS_H";
+  print "#define TL_CONSTANTS_H";
+}
+//  {
+  if (split ($1, a, "#") == 2) {
+    gsub (/[ABCDEFGHIJKLMNOPQRSTUVWXYZ]/, "_&", a[1]);
+    gsub (/[.]/, "_", a[1]);
+    if (a[2] in h) {
+      print "ERROR: Duplicate magic " a[2] " for define " a[1] " and " h[a[2]] >"/dev/stderr/"
+      exit 1;
+    }
+    h[a[2]] = a[1];
+    print "#define", "TL_" toupper(a[1]), "0x" a[2];
+  }
+}
+END {
+  print "#endif";
+}
+"##;
 
     let mut lex = Lexer::new(awk.to_string());
     while let Some(token) = lex.next() {
